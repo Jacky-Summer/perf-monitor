664066b0eb838c2e6591d37ee3bcf61e5989e0a8
JackySummer <1106619871@qq.com>
2022-05-12 21:54:03 +0800

    feat: init project

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {},
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# perf-monitor
+
+性能检测 SDK —— 获取各项性能指标
